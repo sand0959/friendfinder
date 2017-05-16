bd5e7c7d78659e9e3f12f5c68ca89703eec23b23
ted sandridge <sand0959@gmail.com>
2017-05-15 22:05:31 -0400

    wont load

--- server.js
+++ server.js
@@ -6,11 +6,14 @@ var PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json);
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(bodyParser.text());
+
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-require('./app/routing/apiRoutes.js')(app);
-require('./app/routing/htmlRoutes.js')(app);
+
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
 
 app.listen(PORT, function() {
     console.log("Listening on Port " + PORT);
