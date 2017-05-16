34b2e91b268c78723a9ee22b800305ab05739ca8
ted sandridge <sand0959@gmail.com>
2017-05-15 23:53:45 -0400

    fully working and deployed to heroku

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 8000;
 
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.text());
 
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-
+app.use(express.static('app'));
 require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app);
 
